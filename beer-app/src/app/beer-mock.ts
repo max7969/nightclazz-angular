bb078356fd6758ebe6175dbaf4257f4df54bc4fd
Andre Maxime <maxime.andre@zenika.com>
2018-03-25 15:01:43 +0200

    correction images + server

--- beer-app/src/app/beer-mock.ts
+++ beer-app/src/app/beer-mock.ts
@@ -4,28 +4,28 @@ export const BEERS: Beer[] = [
     {
         label: "Queue de Charrue",
         description: "La Queue de Charrue est une famille de bières brassées pour la Brasserie Vanuxeem. La plus connue et typique est la Queue de Charrue brune. Son nom ...",
-        image: "/static/images/queuedecharrue.jpg",
+        image: "/assets/images/queuedecharrue.jpg",
         price: 3.70,
         stock: 2
     },
     {
         label: "La Corbeau",
         description: "La bière du Corbeau est une bière blonde trés gazeuse et avec une belle mousse persistante.Le nez propose des arômes de citron, de végétal et de caramel.la ...",
-        image: "/static/images/corbeau.jpg",
+        image: "/assets/images/corbeau.jpg",
         price: 3.10,
         stock: 2
     },
     {
         label: "Jack Hammer",
         description: "Selon la rumeur, la Jack Hammer serait une bière tellement houblonnée que l'on y retrouverait plus d'amertume que le palais humain ne puisse détecter.",
-        image: "/static/images/jeackhammer.jpg",
+        image: "/assets/images/jeackhammer.jpg",
         price: 3.50,
         stock: 2
     },
     {
         label: "Rince Cochon",
         description: "Autrefois brassée à Annoeullin par la SBA sous le nom de \"Le Rince Cochon\", cette bière est aujourd'hui brassée par la brasserie Haacht, en Belgique, qui ...",
-        image: "/static/images/rincecochon.jpg",
+        image: "/assets/images/rincecochon.jpg",
         price: 3.50,
         stock: 2
     }
