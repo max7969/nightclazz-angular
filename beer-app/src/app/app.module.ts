94839a8f979e593f4d85fa0a1ea16d586f918169
David Da Silva <contact@daviddasilva.net>
2018-03-26 23:24:45 +0200

    ZZzzz

--- beer-app/src/app/app.module.ts
+++ beer-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -24,7 +24,8 @@ import { ProductsComponent } from './products/products.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     BeerService,
--- beer-app/src/app/basket/basket.component.ts
+++ beer-app/src/app/basket/basket.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Beer} from "../beer";
 import {BasketService} from "../basket.service";
+import {Customer} from "../Customer";
 
 @Component({
   selector: 'app-basket',
@@ -11,13 +12,42 @@ export class BasketComponent implements OnInit {
 
   basket: Beer[] = [];
 
-  constructor() {
+  customer: Customer = {  };
 
+  constructor(private basketService: BasketService) {
   }
 
   ngOnInit() {
+    this.basketService.getBasket().subscribe(
+      basket => {
+        this.basket = basket;
+        this.resumerPanier(this.basket);
+      }
+    );
+
 
   }
+
+  validerPanier(){
+    this.basketService.confirmBasket(this.customer);
+  }
+
+
+  resumerPanier(beers : Beer[]) {
+
+    let panier = beers.map((beer, i, b) => {
+      return {
+        label: beer.label,
+        price: beer.price,
+        qte: b.reduce( (accumulateur, valeurCourante) => {
+          return valeurCourante.label === beer.label ? accumulateur +1 : accumulateur;
+        }, 0)
+      }
+    });
+
+    console.log(JSON.stringify(panier));
+  }
+
 
   calculerTotal(): number {
 
